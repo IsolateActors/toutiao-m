75921acbedf40e4abc40e3964bcd1b19b0e4d440
Zon <228657348@qq.com>
2021-02-06 16:50:27 +0800

    项目准备

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.scss'
+import './plugins/vant.js'
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
 
---
+++ src/plugins/vant.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import 'vant/lib/index.css'
+import {
+  Button,
+  NavBar,
+  Tabbar,
+  TabbarItem,
+  Icon
+} from 'vant'
+
+Vue.use(Button)
+Vue.use(NavBar)
+Vue.use(TabbarItem)
+Vue.use(Tabbar)
+Vue.use(Icon)
